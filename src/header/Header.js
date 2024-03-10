9a3f6e6124affc18c04cbff3c043cc8b360d324b
Aiša Dervanović <dervanovicaisa@gmail.com>
2024-03-10 23:33:33 +0100

    init

---
+++ src/header/Header.js
@@ -0,0 +1,26 @@
+import { Nav, Navbar, Image } from "react-bootstrap";
+import "./header.css";
+import Logo from "../assets/aloe-vera-logo.png";
+import { Basket2 } from "react-bootstrap-icons";
+function Header() {
+  return (
+    <Navbar className="d-block border-bottom">
+      <div className="px-5 d-flex justify-content-between align-items-center">
+        <Navbar.Brand href="#home" className="d-flex flex-column">
+          <Image src={Logo} width={60} className="m-auto" alt="Aloe vera" />
+        </Navbar.Brand>
+        <Nav className="aloe-vera-links">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#products">Products</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+        <div className="shopping-cart">
+          <Basket2 />
+        </div>
+      </div>
+    </Navbar>
+  );
+}
+
+export default Header;
---
+++ src/home/Home.js
@@ -0,0 +1,26 @@
+import { Button, Col, Row, Image } from "react-bootstrap";
+import "./home.css";
+import AloeVeraImage from "../assets/cover-image1.jpg";
+function Home() {
+  return (
+    <div>
+      <Row className="home px-0 mx-0 border-bottom">
+        <Col lg={5} className="left-text">
+          <div className="w-75 d-flex flex-column gap-3">
+            <h4 className="title">Feel Comfortable in Flawless Skin</h4>
+            <p className="subtext">
+              Achieve a natural, smooth complecion with Aloe Vera long-lasting,
+              non-oily coverage fundation
+            </p>
+            <Button className="btn-success shop-now w-50">Shop now</Button>
+          </div>
+        </Col>
+        <Col className="px-0 right-text">
+          <Image src={AloeVeraImage} className="image img-responsive" />
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default Home;
