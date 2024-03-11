20f875e03271b0fc6c2c5e3a433a51ba1e821f9f
Aiša Dervanović <dervanovicaisa@gmail.com>
2024-03-11 14:37:12 +0100

    fix

--- src/header/Header.js
+++ src/header/Header.js
@@ -13,7 +13,7 @@ function Header() {
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#about">About</Nav.Link>
           <Nav.Link href="#products">Products</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
+          {/* <Nav.Link href="#contact">Contact</Nav.Link> */}
         </Nav>
         <div className="shopping-cart">
           <Basket2 />
